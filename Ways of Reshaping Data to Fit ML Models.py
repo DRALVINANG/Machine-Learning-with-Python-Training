56b23bc7225ae40cfc99613af3a28b95b7c4856f
Dr Alvin Ang <60359452+DRALVINANG@users.noreply.github.com>
2024-04-02 00:57:14 +0800

    Create Ways of Reshaping Data to Fit ML Models.py

---
+++ Ways of Reshaping Data to Fit ML Models.py
@@ -0,0 +1,26 @@
+#-------------------------------------------------------
+#Using .Reshape
+#-------------------------------------------------------
+#used when
+#'valueerror: expected 2D array, got 1D array instead
+
+X_train = X_train.values.reshape(-1,1)
+X_test = X_test.values.reshape(-1,1)
+
+
+#-------------------------------------------------------
+#Using pd.DataFrame
+#-------------------------------------------------------
+#used when
+#'valueerror: expected 2D array, got 1D array instead
+
+X_train = pd.DataFrame(X_train)
+X_test = pd.DataFrame(X_test)
+
+#-------------------------------------------------------
+#Using .flatten()
+#-------------------------------------------------------
+#used when
+#valueerror: x and y must have same first dimension, but
+#have shapes (100,) and (1,100)
+
